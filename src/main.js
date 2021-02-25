1a5650d0f990603a272d42e41f1e8406d64def36
George Kolev <gogo1520180@gmail.com>
2021-02-25 18:07:35 +0200

    wtf

--- src/main.js
+++ src/main.js
@@ -4,8 +4,14 @@ import settings from "../data/theme.json";
 import "@/assets/code-highlight.css"
 
 export default function(Vue, { head }) {
+
   Vue.component("Layout", DefaultLayout);
   head.bodyAttrs = {
     class: settings.dark_mode ? "dark" : ""
   };
+  
+  head.script.push({
+    src:"<script data-goatcounter='https://gekolev.goatcounter.com/count' async src='//gc.zgo.at/count.js'></script>",
+    body: true})
+
 }
